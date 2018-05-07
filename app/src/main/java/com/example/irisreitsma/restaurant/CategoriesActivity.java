81cbaf134a315d905902b493e96f9efe87a9ff95
Iris Reitsma <irisreitsma@online.nl>
2018-05-07 11:14:09 +0200

    2/3 done

--- app/src/main/java/com/example/irisreitsma/restaurant/CategoriesActivity.java
+++ app/src/main/java/com/example/irisreitsma/restaurant/CategoriesActivity.java
@@ -1,13 +1,59 @@
 package com.example.irisreitsma.restaurant;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class CategoriesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CategoriesActivity extends AppCompatActivity implements CategoriesRequest.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        // get categories
+        CategoriesRequest request = new CategoriesRequest(this);
+        request.getCategories(this);
+    }
+
+    @Override
+    // attach adapter to listview
+    public void gotCategories(ArrayList<String> categories) {
+        ArrayAdapter<String> category = new CategoryAdapter(this, R.layout.entry_row, categories);
+        ListView lv = findViewById(R.id.listview);
+        lv.setAdapter(category);
+        lv.setOnItemClickListener(new OnItemClickListener());
+    }
+
+    @Override
+    // make toast when error
+    public void gotCategoriesError(String message) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast.makeText(context, message, duration).show();
+    }
+
+    // when category is clicked open menuactivity
+    public class OnItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            // get clicked category
+            String category = (String) adapterView.getItemAtPosition(i);
+
+            // open menuactivity
+            Intent intent = new Intent(CategoriesActivity.this, MenuActivity.class);
+            intent.putExtra("category", category);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/irisreitsma/restaurant/MenuItemAdapter.java
@@ -0,0 +1,55 @@
+package com.example.irisreitsma.restaurant;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Iris Reitsma on 6-5-2018.
+ */
+
+public class MenuItemAdapter extends ArrayAdapter<MenuItem> {
+
+    private ArrayList<MenuItem> menuItems;
+
+    public MenuItemAdapter(@NonNull Context context, int resource, @NonNull ArrayList<MenuItem> menuItems) {
+        super(context, resource, menuItems);
+        this.menuItems = menuItems;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        // set image
+        ImageView imageView = convertView.findViewById(R.id.image);
+        new DownloadImage(imageView).execute(menuItems.get(position).getImageUrl());
+
+        // set name
+        TextView name = convertView.findViewById(R.id.name);
+        name.setText(menuItems.get(position).getName());
+
+        // set price
+        TextView price = convertView.findViewById(R.id.price);
+        price.setText("€ " + String.format("%.2f", menuItems.get(position).getPrice()));
+
+//        // dynamically set row height of grid view
+//        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+//        int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+//        int itemHeight = screenHeight > screenWidth ? screenHeight / 7 : screenWidth / 7;
+//        convertView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, itemHeight));
+
+        return convertView;
+    }
+}
